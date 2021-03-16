0303f50cbd528d99960b20890aea53318275915b
Navneet007 <navneetjustin@gmail.com>
2021-03-16 16:10:51 +0530

    push

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, TouchableWithoutFeedback, Alert, Button } from 'react-native';
 
 export default function App() {
+  console.log("App executed")
+  let x = 1; 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>first recheck react navtive app!</Text>
+      <TouchableHighlight onPress={() => console.log("did you tapped on image")}>
+
+      <Image source= {{ 
+        width:200,
+        height:300,
+        uri: "https://picsum.photos/200/300"
+        }}/>
+      </TouchableHighlight>
+       <Button color="orange" title="Click me" 
+       onPress={() => 
+        Alert.alert("my title", "my messages", [
+         {text:"Yes", onPress: () => console.log("yes") },
+         {text:"No", onPress: () => console.log("No") },
+         ])} />
+         <Button color="orange" title="Click me for input" 
+       onPress={() => 
+        Alert.prompt("my title", "my messages", text => console.log(text))} />
     </View>
   );
 }
@@ -14,7 +32,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
